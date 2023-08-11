19a7d247ae283d20b0cf4e2a6ed9e46dc61947c8
ghaith31 <gharsallighaith31@gmail.com>
2023-08-11 14:11:17 +0200

    unique

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,7 @@
--- Creates the table unique_id.
-CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id`   INT          DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256)
+-- creates the table unique_id with the following requirements:
+-- id INT with the default value of 1 and unique
+-- name VARCHAR(256)
+CREATE TABLE unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
 );
